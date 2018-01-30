1726f8f87472a6371b23231ae656a3e0c62458d1
mirvakataja <mirva.kataja@me.com>
2018-01-30 19:53:03 +0200

    fdsf
    sdfsdf

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
-var jani = require('./routes/jani');
+var mirva = require('./routes/mirva');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/about', about);
-app.use('/jani', jani);
+app.use('/mirva', mirva);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/mirva.js
+++ routes/mirva.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('My deployment github respository can be found at https://github.com/jani5454/DeployToAzure');
+  res.send('My deployment github respository can be found at https://github.com/mirvakataja/DeployToAzure');
 });
 
 module.exports = router;
